e208dc1686b323b77221d1406f1bc9ab8eab4a49
Gopal Shukla <gopalshukla0018@gmail.com>
2025-10-30 22:20:58 +0530

    for using range

--- 05_for/for.go
+++ 05_for/for.go
@@ -4,15 +4,29 @@ import "fmt"
 
 func main() {
 	// in go we have not while loop so we use for as while loop
-	i := 0
-	for i <= 10 {
-		fmt.Println(i)
-		i++
+	// i := 0
+	// for i <= 10 {
+	// 	fmt.Println(i)
+	// 	i++
 
-	}
+	// }
 
 	// infinite loop
-	for {
-		// fmt.Println("Gopal")
+	// for {
+	// 	fmt.Println("Gopal")
+	// }
+
+	// classoc for loop
+	// for j := 0; j <=3; j++ {
+	// 	// break
+	// 	// continue
+	// 	fmt.Println(j)
+	// }
+
+	// using range--
+	// 1.22 me aya ye
+	for i := range 10 {
+		fmt.Println(i)
 	}
+
 }
