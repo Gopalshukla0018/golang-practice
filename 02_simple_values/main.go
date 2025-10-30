acebe4763e551e8b9cc9aceeb2564487817ed50c
Gopal Shukla <gopalshukla0018@gmail.com>
2025-10-30 12:20:39 +0530

    chapter 2 values

---
+++ 02_simple_values/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int
+	fmt.Println(10)
+
+	// string
+	fmt.Println("Gopal shukla")
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+	fmt.Println(true, false)
+
+	//float
+	fmt.Println(10.2)
+
+	// we can do arithmetic op..
+	fmt.Println(1 + 5)
+
+}
