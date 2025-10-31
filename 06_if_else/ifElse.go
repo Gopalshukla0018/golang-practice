f09f1978e32027076355d2f18e298effda535694
Gopal Shukla <gopalshukla0018@gmail.com>
2025-10-31 20:15:50 +0530

    06 if else completed

---
+++ 06_if_else/ifElse.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// age := 18
+
+	// if age >= 18 {
+	// 	fmt.Println("you can drive car")
+	// } else {
+	// 	fmt.Println("You can not drive car")
+	// }
+
+	// var role = "admin"
+	// var isAdmin = false
+	// if role == "admin" && isAdmin {
+	// 	fmt.Println("you are authorised")
+	// }
+
+	// we can define variable in if
+	if age := 19; age >= 18 {
+		fmt.Println("You can  drive car")
+	}
+
+	// go doesn't have ternary operator
+}
