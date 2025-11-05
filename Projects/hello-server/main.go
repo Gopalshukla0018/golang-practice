ea1f45ad3e1f96ba28c9f05ab32b3c8de8b80295
Gopal Shukla <gopalshukla0018@gmail.com>
2025-11-05 10:04:03 +0530

    make a basic go server

---
+++ Projects/hello-server/main.go
@@ -0,0 +1,19 @@
+package main
+
+//  making a simple server
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from backend!")
+	})
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Home page")
+	})
+
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
